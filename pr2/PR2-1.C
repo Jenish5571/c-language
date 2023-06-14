fb5da2019d791ca2cf8583ef8c4d578019c9fb91
jenish5571 <jenishhirapara89@gmail.com>
2023-06-14 17:04:22 +0530

    pr2

---
+++ pr2/PR2-1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+
+	clrscr();
+	printf("Enter a : ");
+	scanf("%d",&a);
+	(a%2 == 15)
+		?printf("this number is odd ")
+		:printf("this number is even");
+
+	getch();
+}
