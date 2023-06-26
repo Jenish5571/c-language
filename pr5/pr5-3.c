75a27247bbc86e175bf00e25a2d028e47cb57442
jenish5571 <jenishhirapara89@gmail.com>
2023-06-26 17:26:54 +0530

    pr5

---
+++ pr5/pr5-3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter row and column: ");
+	scanf("%d",&n);
+	int a[n][n],i,j;
+
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+		printf("a[%d][%d]: ",i,j);
+		scanf("%d",&a[i][j]);
+		}	
+	}
+	printf("Transpose of given matrix: \n");  
+    for(i=0; i<n; i++)
+	{  
+        for(j=0; j<n; j++)
+		{  
+           printf("%d ",a[i][j]);  
+        }  
+        printf("\n");  
+    }  
+}
+
